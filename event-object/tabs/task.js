736d94a94c93975930972089ba34f7f79b66d821
B-Mikhail-V <bakulin.mv@gmail.com>
2022-10-02 21:37:11 +0300

    Task_3_сщкк

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,25 +1,26 @@
 // массив вкладок
 const tabs = Array.from(document.querySelectorAll('.tab'));
 
+
 // находим индекс активной вкладки
 function indexActiveTab() {
   return tabs.findIndex(tab => tab.classList.contains('tab_active'));
 }
 
+
 // массив контента
 const content = Array.from(document.querySelectorAll('.tab__content'));
+
 
 // активация вкладки по индексу вкладки
 function tabActivate(index) {
     tabs[index].classList.add('tab_active');
 }
 
-// деактивация всех вкладки
-function tabDeactivate() {
-    tabs.forEach(element => {
-        element.classList.remove('tab_active');
 
-    })
+// деактивация вкладки по индексу
+function tabDeactivate(index) {
+    tabs[index].classList.remove('tab_active')
     
 }
 
@@ -29,21 +30,31 @@ function contentActivate(index) {
     content[index].classList.add('tab__content_active');
 }
 
-// деактивация контента по индексу вкладки
-function contentDeactivate() {
-    content.forEach(element => {
-        element.classList.remove('tab__content_active');
 
+// деактивация контента, относящегося к группе вкладов
+function contentDeactivate(tabParent) {
+    content.forEach(element => {
+        if(element.closest('.tabs').id == tabParent) {
+            element.classList.remove('tab__content_active');
+        }
+        
     })
     
 }
 
 
-// обработка события
+// определяем id группы вкладок, на элемент которой клюкнули
+function tabNumber(element) {
+    const tabsId = element.closest('.tabs').id
+    return tabsId;
+}
+
+
+// обработка событий
 tabs.forEach(element => {
     element.addEventListener('click', () => {
-        tabDeactivate();
-        contentDeactivate();
+        tabDeactivate(indexActiveTab());
+        contentDeactivate(tabNumber(element));
         const indexClicked = tabs.indexOf(element);        
         tabActivate(indexClicked);
         contentActivate(indexClicked);
